0d683466655366bacedf24f3f2db7072a398e4dc
Alexander Herbst <git@familie-herbst.net>
2015-04-07 23:00:14 +0200

    Some commit message

--- ext/class.backup.php
+++ ext/class.backup.php
@@ -12,13 +12,13 @@
       if (isset($output)) unset($output);
       exec('"' . $extConf['mysqlDumpPath'] . '" --host=' . TYPO3_db_host . ' --user=' . TYPO3_db_username . ' typo3 > ' . PATH_site . 'typo3temp/typo3_dump.sql', $output);
 
-//       $mail = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Mail\\MailMessage');
-//       $mail->setFrom(array('alexander.herbst@inapa-packaging.de' => 'Alexander Herbst'))
-//            ->setTo(array('alexander.herbst@inapa-packaging.de' => 'Alexander Herbst'))
-//            ->setSubject('backup')
-//            ->setBody(implode("\n", $output))
-//            ->send();
-//      
+      $mail = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Mail\\MailMessage');
+      $mail->setFrom(array('alexander.herbst@inapa-packaging.de' => 'Alexander Herbst'))
+           ->setTo(array('alexander.herbst@inapa-packaging.de' => 'Alexander Herbst'))
+           ->setSubject('backup')
+           ->setBody(implode("\n", $output))
+           ->send();
+     
       ahx_lib::writeLog(4, 0, 0, 0, '[ahx_backup] Datenbanksicherung beendet.', 0);
       ahx_lib::writeLog(4, 0, 0, 0, '[ahx_backup] Datensicherung beginnt.', 0);
 
